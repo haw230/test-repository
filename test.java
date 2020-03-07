28eb69a5ac4a051dda1f6c1fb39f8b15e18ed1a4
Han Wu <hancheng.wu22@gmail.com>
2020-03-07 16:59:10 -0500

    Update test.java

--- test.java
+++ test.java
@@ -6,8 +6,13 @@ dsffdssddfssdf
 for(int i = 0; ; ;) {}
 
 
-public int a() {}
+public int a(bool blah) {}
 
-private string b(int a){}
+private string b(int a, string a, string b){}
 
-private string c(int a, bool hi){}
+private string c(int a){}
+
+dfsdf
+
+sdfsdf
+//hiiiii
